5aef4090b82b0cec80a1f66734aac3cb5299c19c
nicoraww <94559409+nicoraww@users.noreply.github.com>
2025-02-27 07:18:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Mi Primera App")
+
+st.header("Portfoio NicoRaw")
+st.write("IXD DESIGNER")
+image = Image.open('Papi_.png)
+
+st.image(image, caption="Pic")
