3ea2dc906dd8efd5fff9be7b0a1321b7f87e3a0c
nicoraww <94559409+nicoraww@users.noreply.github.com>
2025-02-27 08:11:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,18 @@ with col1:
     st.write("Correcto")
 
 with col2:
-  st.subheader("Esta es la Segunda Columna")
+    st.subheader("Esta es la segunda columna")
+    
+    modo = st.radio("Que Modalidad es la principal en tu interfaz", ('Visual', 'auditiva', 'Táctil'))
+    
+    if modo == 'Visual':
+        st.write('La vista es fundamental para tu interfaz')
+    
+    if modo == 'auditiva':
+        st.write('La audición es fundamental para tu interfaz')
+    
+    if modo == 'Táctil':
+        st.write('El tacto es fundamental para tu interfaz')
 
 st.subheader("Botones")
 
