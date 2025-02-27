bc0bd4c280e078a722e81a7a3e10cb4e8437e78c
nicoraww <94559409+nicoraww@users.noreply.github.com>
2025-02-27 07:22:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Mi Primera App")
 
 st.header("Portfoio NicoRaw")
 st.write("IXD DESIGNER")
-image = Image.open('Papi_.png)
+image = Image.open("Papi_.png")
 
 st.image(image, caption="Pic")
